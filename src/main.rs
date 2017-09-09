66d7c7e0af4e20c0fa7dcd808fa477ccc92c970a
Sumner Evans <sumner.evans98@gmail.com>
2017-09-09 17:27:12 -0600

    logging and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 // Crates
+#[macro_use]
+extern crate log;
+
 extern crate rust_webvr;
 extern crate clap;
+extern crate simplelog;
 
 // Libraries
 use rust_webvr::VRServiceManager;
-use clap::{Arg, App, SubCommand};
+use rust_webvr::api::OpenVRServiceCreator;
+
+use simplelog::TermLogger;
+use clap::{Arg, App};
 
 fn main() {
+    // logging setup
+    TermLogger::init(LogLevelFilter::Warn, Config::default());
+
     // check for mock
     let matches = App::new("VR")
         .arg(Arg::with_name("mock")
             .short("m")
             .long("mock")
-            .help("Use mock vr api"))
+            .help("Use mock VR API"))
         .get_matches();
-    
+
     // Init
     let mut vrsm = VRServiceManager::new();
     if matches.is_present("mock") {
@@ -23,6 +33,11 @@ fn main() {
         vrsm.register_defaults();
     }
 
-    println!("{}", vrsm.get_displays().len());
+    println!("{:?}", vrsm.get_displays());
 
+    // Select first display
+    let display = displays.get(0).unwrap();
+
+    let display_data = display.borrow().data();
+    println!("VRDisplay: {:?}", display_data);
 }
