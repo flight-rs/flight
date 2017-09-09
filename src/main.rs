d46f7d0fbe7256d040b807cd2e2fbda6ddd838cb
Robinson Merillat <rdmerillat@mymail.mines.edu>
2017-09-09 14:00:53 -0600

    Created Mock Display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// Crates
+extern crate rust_webvr;
+extern crate clap;
+
+// Libraries
+use rust_webvr::VRServiceManager;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    // check for mock
+    let matches = App::new("VR")
+        .arg(Arg::with_name("mock")
+            .short("m")
+            .long("mock")
+            .help("Use mock vr api"))
+        .get_matches();
+    
+    // Init
+    let mut vrsm = VRServiceManager::new();
+    if matches.is_present("mock") {
+        vrsm.register_mock();
+    } else {
+        vrsm.register_defaults();
+    }
+
+    println!("{}", vrsm.get_displays().len());
+
 }
